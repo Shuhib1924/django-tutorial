0a012462273ff4b474434e57c2c59a53d9838ed6
Shuhib <shuhib_s@live.de>
2025-03-09 10:40:36 +0100

    middleware by request path

--- middlewares/middleware.py
+++ middlewares/middleware.py
@@ -5,6 +5,10 @@ class CustomClassMiddleware:
 
     def __call__(self, request):
         print("Before view")
+        # if request.path == "/middlewares/":
+        #     print(f"middleware block - {request.path}")
+        if "middle" or "ware" in request.path:
+            print(f"middleware block - {request.path}")
         response = self.get_response(request)
         print("After view")
         return response
