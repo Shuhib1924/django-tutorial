ce94c02564e7004016e5f18c966475e25a83a9b1
Shuhib <shuhib_s@live.de>
2025-03-09 11:05:26 +0100

    middleware by process view

--- middlewares/middleware.py
+++ middlewares/middleware.py
@@ -12,3 +12,13 @@ class CustomClassMiddleware:
         response = self.get_response(request)
         print("After view")
         return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        print("call process_view")
+        print(f"request.method - {request.method}")
+        print(f"request.path - {request.path}")
+        print(f"view_func.__name__  -.{view_func.__name__}")
+        print(f"view_func.__module__  -.{view_func.__module__}")
+        print(f"view_args - {view_args}")
+        print(f"view_kwargs - {view_kwargs}")
+        return None
